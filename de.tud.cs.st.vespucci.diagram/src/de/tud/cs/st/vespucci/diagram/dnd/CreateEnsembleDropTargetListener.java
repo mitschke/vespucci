df1cf1c1f4ea7bff22b7c452df99a54f4658b914
Malte <Malte@local>
2011-02-05 16:38:36 +0100

    small fixes

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/dnd/CreateEnsembleDropTargetListener.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/dnd/CreateEnsembleDropTargetListener.java
@@ -1,19 +1,14 @@
 package de.tud.cs.st.vespucci.diagram.dnd;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.Request;
-import org.eclipse.gef.requests.DirectEditRequest;
-import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequestFactory;
 import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.dnd.DropTargetEvent;
 
 import de.tud.cs.st.vespucci.vespucci_model.Vespucci_modelPackage;
