8680e4ddf60b193c8beb2fbb2b33659d928b1510
Artem Vovk <vovk.artem@gmail.com>
2011-02-23 16:08:47 +0100

    added editparts

---
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/vespucci_model/diagram/edit/parts/EnsembleEnsembleCompartment2EditPart.java
@@ -0,0 +1,169 @@
+/*
+ *  License (BSD Style License):
+ *   Copyright (c) 2010
+ *   Author Tam-Minh Nguyen
+ *   Software Engineering
+ *   Department of Computer Science
+ *   Technische Universit�t Darmstadt
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Engineering Group or Technische 
+ *     Universit�t Darmstadt nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this software without specific 
+ *     prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.tud.cs.st.vespucci.vespucci_model.diagram.edit.parts;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableCompartmentEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.gmf.runtime.notation.View;
+
+import de.tud.cs.st.vespucci.diagram.supports.CompartmentEditPartSupporter;
+
+/**
+ * @generated
+ */
+public class EnsembleEnsembleCompartment2EditPart extends
+		ShapeCompartmentEditPart {
+
+	/**
+	 * @generated
+	 */
+	public static final int VISUAL_ID = 7002;
+
+	private CompartmentEditPartSupporter abm;
+
+	/**
+	 * @generated NOT
+	 */
+	public EnsembleEnsembleCompartment2EditPart(View view) {
+		super(view);
+
+		//
+		abm = new CompartmentEditPartSupporter(this);
+	}
+
+	/**
+	 * @generated
+	 */
+	public String getCompartmentName() {
+		return de.tud.cs.st.vespucci.vespucci_model.diagram.part.Messages.EnsembleEnsembleCompartment2EditPart_title;
+	}
+
+	/**
+	 * @generated
+	 */
+	public IFigure createFigure() {
+		ResizableCompartmentFigure result = (ResizableCompartmentFigure) super
+				.createFigure();
+		result.setTitleVisibility(false);
+		return result;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE,
+				new ResizableCompartmentEditPolicy());
+		installEditPolicy(
+				EditPolicyRoles.SEMANTIC_ROLE,
+				new de.tud.cs.st.vespucci.vespucci_model.diagram.edit.policies.EnsembleEnsembleCompartment2ItemSemanticEditPolicy());
+		installEditPolicy(EditPolicyRoles.CREATION_ROLE,
+				new CreationEditPolicy());
+		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE,
+				new DragDropEditPolicy());
+		installEditPolicy(
+				EditPolicyRoles.CANONICAL_ROLE,
+				new de.tud.cs.st.vespucci.vespucci_model.diagram.edit.policies.EnsembleEnsembleCompartment2CanonicalEditPolicy());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void setRatio(Double ratio) {
+		if (getFigure().getParent().getLayoutManager() instanceof ConstrainedToolbarLayout) {
+			super.setRatio(ratio);
+		}
+	}
+
+	/**
+	 * @generated NOT
+	 */
+	@Override
+	protected void handleNotificationEvent(Notification event) {
+		super.handleNotificationEvent(event);
+		// ShapeImpl tmpl = (ShapeImpl) this.getParent().getModel();
+		// Shape t = (Shape) tmpl.getElement();
+		// // NotationPackage.eINSTANCE.get
+		// System.out.println("Notific: " + t.getName());
+		// System.out.println("Event:" + event.getFeature());
+		// TODO: delete/move connection???
+		if (NotationPackage.eINSTANCE.getDrawerStyle_Collapsed().equals(
+				event.getFeature())) {
+			this.abm.updateConnections(event);
+		}
+//		else if (event.getEventType() == Notification.REMOVE) {
+//			if (NotationPackage.eINSTANCE.getView_TargetEdges().equals(
+//					event.getFeature())) {
+//				if (event.getOldValue() instanceof EdgeImpl) {
+//					EdgeImpl edge = (EdgeImpl) event.getOldValue();
+//					if (edge.getElement() != null) {
+//						Connection con = (Connection) edge.getElement();
+//						if (con.getOriginalTarget() != null) {
+//							con.setOriginalTarget(null);
+//							System.out.println("Ziehen target");
+//						}
+//					}
+//
+//				}
+//
+//			} else if (NotationPackage.eINSTANCE.getView_SourceEdges().equals(
+//					event.getFeature())) {
+//				if (event.getOldValue() instanceof EdgeImpl) {
+//					EdgeImpl edge = (EdgeImpl) event.getOldValue();
+//					if (edge.getElement() != null) {
+//						Connection con = (Connection) edge.getElement();
+//						if (con.getOriginalSource() != null) {
+//							con.setOriginalSource(null);
+//							System.out.println("Ziehen source");
+//						}
+//					}
+//
+//				}
+//
+//			}
+//		}
+
+	}
+}
