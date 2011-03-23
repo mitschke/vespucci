9a8d121a4eebabbd2c16727681c9fc75ec7de8bc
Malte <Malte@local>
2011-03-23 15:26:39 +0100

    readd bug fix

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/converter/DiagramConverter.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/converter/DiagramConverter.java
@@ -354,7 +354,11 @@ public class DiagramConverter {
 	 * @author Patrick Jahnke
 	 */
 	private String getEnsembleDescriptor(Shape shape) {
-		String name = shape.getName().length()== 0 ? "non-editpart" : shape.getName();
+		String name;
+		if(shape.getName() == null)
+			name = "";
+		else
+		 name = shape.getName().length()== 0 ? "non-editpart" : shape.getName();
 		StringBuilder s = new StringBuilder("'");
 		if (name.indexOf('(') > 0) {
 			s.append(name.subSequence(0, name.indexOf('(')));
@@ -406,7 +410,6 @@ public class DiagramConverter {
 	 * @author Patrick Jahnke
 	 */
 	private String[] getEnsembleParameterDefinitions(Ensemble ensemble) {
-		// FIXME BenjaminL: schneller hack, dass keine nullpointerexception beim speichern eines diagramms mit einem ensemble mit leeren namen auftritt - schöner kettensatz, he?
 		if(ensemble.getName()==null){
 			ensemble.setName("");
 		}
