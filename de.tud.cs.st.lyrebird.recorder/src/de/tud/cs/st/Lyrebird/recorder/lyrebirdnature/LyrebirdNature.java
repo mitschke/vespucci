a0c5632be2ba9337ac2e0b64b95308b61bad95b3
Malte <me@local>
2011-05-31 20:22:29 +0200

    Change-Id: Id721c56a76eccf8d84e7cb60272e3243c93d5e37

---
+++ de.tud.cs.st.lyrebird.recorder/src/de/tud/cs/st/Lyrebird/recorder/lyrebirdnature/LyrebirdNature.java
@@ -0,0 +1,68 @@
+package de.tud.cs.st.Lyrebird.recorder.lyrebirdnature;
+
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class LyrebirdNature implements IProjectNature {
+
+	/**
+	 * ID of this project nature
+	 */
+	public static final String NATURE_ID = "Lyrebird.LyrebirdNatureID";
+
+	private IProject project;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#configure()
+	 */
+	public void configure() throws CoreException {
+//		IProjectDescription desc = project.getDescription();
+//		ICommand[] commands = desc.getBuildSpec();
+//
+//		for (int i = 0; i < commands.length; ++i) {
+//			if (commands[i].getBuilderName().equals(LyrebirdBuilder.BUILDER_ID)) {
+//				return;
+//			}
+//		}
+//
+//		ICommand[] newCommands = new ICommand[commands.length + 1];
+//		System.arraycopy(commands, 0, newCommands, 0, commands.length);
+//		ICommand command = desc.newCommand();
+//		command.setBuilderName(LyrebirdBuilder.BUILDER_ID);
+//		newCommands[newCommands.length - 1] = command;
+//		desc.setBuildSpec(newCommands);
+//		project.setDescription(desc, null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#deconfigure()
+	 */
+	public void deconfigure() throws CoreException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#getProject()
+	 */
+	public IProject getProject() {
+		return project;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core.resources.IProject)
+	 */
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+}
