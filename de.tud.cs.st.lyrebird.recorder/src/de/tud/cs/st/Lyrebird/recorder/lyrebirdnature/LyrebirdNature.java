5fa8f21733fb462c21a9ddd23623c12c7e6e4020
Malte <me@local>
2011-06-02 13:20:37 +0200

    add BSD License to all files beautification preferencepage
    Change-Id: I1a112ae64efa278eac9cbf51a3438e3e9ba2b457

--- de.tud.cs.st.lyrebird.recorder/src/de/tud/cs/st/Lyrebird/recorder/lyrebirdnature/LyrebirdNature.java
+++ de.tud.cs.st.lyrebird.recorder/src/de/tud/cs/st/Lyrebird/recorder/lyrebirdnature/LyrebirdNature.java
@@ -1,3 +1,34 @@
+/* License (BSD Style License):
+ * Copyright (c) 2011
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the Software Technology Group or Technische 
+ *    Universität Darmstadt nor the names of its contributors may be used to 
+ *    endorse or promote products derived from this software without specific 
+ *    prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st.Lyrebird.recorder.lyrebirdnature;
 
 import org.eclipse.core.resources.ICommand;
@@ -6,6 +37,10 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
+/**
+ * ProjectNature class of the Lyrebird.recorder project
+ * @author Malte V
+ */
 public class LyrebirdNature implements IProjectNature {
 
 	/**
@@ -15,53 +50,18 @@ public class LyrebirdNature implements IProjectNature {
 
 	private IProject project;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.resources.IProjectNature#configure()
-	 */
+
 	public void configure() throws CoreException {
-//		IProjectDescription desc = project.getDescription();
-//		ICommand[] commands = desc.getBuildSpec();
-//
-//		for (int i = 0; i < commands.length; ++i) {
-//			if (commands[i].getBuilderName().equals(LyrebirdBuilder.BUILDER_ID)) {
-//				return;
-//			}
-//		}
-//
-//		ICommand[] newCommands = new ICommand[commands.length + 1];
-//		System.arraycopy(commands, 0, newCommands, 0, commands.length);
-//		ICommand command = desc.newCommand();
-//		command.setBuilderName(LyrebirdBuilder.BUILDER_ID);
-//		newCommands[newCommands.length - 1] = command;
-//		desc.setBuildSpec(newCommands);
-//		project.setDescription(desc, null);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.resources.IProjectNature#deconfigure()
-	 */
 	public void deconfigure() throws CoreException {
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.resources.IProjectNature#getProject()
-	 */
 	public IProject getProject() {
 		return project;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core.resources.IProject)
-	 */
 	public void setProject(IProject project) {
 		this.project = project;
 	}
--- de.tud.cs.st.lyrebird.recorder/src/de/tud/cs/st/Lyrebird/recorder/preferences/PreferencePage.java
+++ de.tud.cs.st.lyrebird.recorder/src/de/tud/cs/st/Lyrebird/recorder/preferences/PreferencePage.java
@@ -1,29 +1,54 @@
+/* License (BSD Style License):
+ * Copyright (c) 2011
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the Software Technology Group or Technische 
+ *    Universität Darmstadt nor the names of its contributors may be used to 
+ *    endorse or promote products derived from this software without specific 
+ *    prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st.Lyrebird.recorder.preferences;
 
 import org.eclipse.jface.preference.*;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.IWorkbench;
 
 import de.tud.cs.st.Lyrebird.recorder.Activator;
 
 /**
- * This class represents a preference page that
- * is contributed to the Preferences dialog. By 
- * subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows
- * us to create a page that is small and knows how to 
- * save, restore and apply itself.
- * <p>
- * This page is used to modify preferences only. They
- * are stored in the preference store that belongs to
- * the main plug-in class. That way, preferences can
- * be accessed directly via the preference store.
+ * 
+ * @author Malte V
  */
-
 public class PreferencePage
 	extends FieldEditorPreferencePage
 	implements IWorkbenchPreferencePage {
 
+	private StringFieldEditor prefFoldername;
+	private DirectoryFieldEditor prefPath;
+	
 	public PreferencePage() {
 		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
@@ -37,15 +62,36 @@ public class PreferencePage
 	 * restore itself.
 	 */
 	public void createFieldEditors() {
+		
 		addField(
 				new BooleanFieldEditor(
 					PreferenceConstants.P_SAVE_PAIR_PROJECT,
 					"Save output separately for every project",
 					getFieldEditorParent()));
-		addField(
-				new StringFieldEditor(PreferenceConstants.P_PROJECT_RELATIV_PATH, "Output folder (folder name must be unique)(if output is saved separately fore every project):", getFieldEditorParent()));
-		addField(new DirectoryFieldEditor(PreferenceConstants.P_ABSOLUTE_PATH, 
-				"Output path(if logs are saved for all projects in one folder):", getFieldEditorParent()));
+		prefFoldername = new StringFieldEditor(PreferenceConstants.P_PROJECT_RELATIV_PATH, "Output folder (folder name must be unique)(if output is saved separately fore every project):", getFieldEditorParent());
+		addField(prefFoldername);
+		prefPath = new DirectoryFieldEditor(PreferenceConstants.P_ABSOLUTE_PATH, 
+				"Output path(if logs are saved for all projects in one folder):", getFieldEditorParent());
+		prefPath.setEnabled(false, getFieldEditorParent());
+		addField(prefPath);
+	}
+	
+	
+	 @Override
+	public void propertyChange(PropertyChangeEvent event) {
+		 //toggle the enable property of StringFieldEditor and DirectoryFieldEditor if the value of BooleanFieldEditor changes
+		 if(event.getSource() instanceof org.eclipse.jface.preference.BooleanFieldEditor){
+			 if(((BooleanFieldEditor) event.getSource()).getPreferenceName().equals(PreferenceConstants.P_SAVE_PAIR_PROJECT)){
+				 if(event.getNewValue().equals(true)){
+					 prefPath.setEnabled(false, getFieldEditorParent());
+					 prefFoldername.setEnabled(true, getFieldEditorParent());
+				 }else{
+					 prefPath.setEnabled(true, getFieldEditorParent());
+					 prefFoldername.setEnabled(false, getFieldEditorParent());
+				 }
+			 }
+		 }
+		super.propertyChange(event);
 	}
 
 	/* (non-Javadoc)
