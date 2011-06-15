c11d3704e76f66b8a767d57c433bfb97029cb6c6
Dominic Scheurer <dom-scheurer@gmx.de>
2011-06-15 21:45:26 +0200

    Added Transformation (QVT OML) and context menu item for executing it. Kind of works, but in transformed output there is no diagram notation contained, only ShapesDiagram part.

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/actions/TransformVespucciV0ToV1.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/actions/TransformVespucciV0ToV1.java
@@ -1,15 +1,19 @@
 package de.tud.cs.st.vespucci.diagram.actions;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.m2m.qvt.oml.runtime.ui.wizards.RunInterpretedTransformationWizardDelegate;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
@@ -30,12 +34,14 @@ public class TransformVespucciV0ToV1 implements IObjectActionDelegate {
 				MessageDialog.openInformation(getShell(), title, NLS.bind(message, fileURI.toString()));
 			} else {
 				URI transfUri = URI.createURI(
-					"platform:/de.tud.cs.st.vespucci.transforms/transformations/migrate_v0_to_v1.qvto"
+					"platform:/de.tud.cs.st.vespucci/transformations/migrate_v0_to_v1.qvto"
 				); //$NON-NLS-1$
 				ArrayList<URI> paramUris = new ArrayList<URI>();
 				paramUris.add(fileURI);
 				
-				//IWizard wizard = (IWizard) new RunI
+				IWizard wizard = (IWizard) new RunInterpretedTransformationWizardDelegate(transfUri, paramUris);
+				WizardDialog wizardDialog = new WizardDialog(getShell(), wizard);
+				wizardDialog.open();
 			}
 		} catch (Exception ex) {
 			handleError(ex);
@@ -43,18 +49,16 @@ public class TransformVespucciV0ToV1 implements IObjectActionDelegate {
 	}
 
 	private void handleError(Exception ex) {
-		// TODO Auto-generated method stub
-		
+		MessageDialog.openError(getShell(), "Transformation failed", MessageFormat.format("{0}: {1}", ex.getClass().getSimpleName(), ex.getMessage() == null ? "no message" : ex.getMessage()));
 	}
 
 	private Shell getShell() {
-		// TODO Auto-generated method stub
-		return null;
+		return targetPart.getSite().getShell();
 	}
 
 	private EObject getInput() {
-		// TODO Auto-generated method stub
-		return null;
+		ResourceSetImpl rs = new ResourceSetImpl();
+		return rs.getEObject(fileURI.appendFragment("/"), true);
 	}
 
 	@Override
