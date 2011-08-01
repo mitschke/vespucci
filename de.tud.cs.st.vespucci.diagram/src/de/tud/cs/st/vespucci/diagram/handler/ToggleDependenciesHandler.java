b921ae82387dd5fa986f52f83a992d3110d4ded9
Alexander Weitzmann <alexander.weitzmann@gmx.de>
2011-08-01 14:12:22 +0200

    Revert "Removed popUpMenu-additions. (Going to be readded.)"
    This reverts commit 3f73cf92861134cf0416efaae4d6c82669fc6d35.

---
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/handler/ToggleDependenciesHandler.java
@@ -0,0 +1,97 @@
+/**
+ *  License (BSD Style License):
+ *   Copyright (c) 2010
+ *   Software Engineering
+ *   Department of Computer Science
+ *   Technische Universität Darmstadt
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Engineering Group or Technische 
+ *     Universität Darmstadt nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this software without specific 
+ *     prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.tud.cs.st.vespucci.diagram.handler;
+
+import java.util.Arrays;
+
+/**
+ * Handler for the "Toggle Dependency"-command.<br>
+ * "Toggle Dependency" is found in the popup-menu for constraints (connections in editor) under
+ * "Edit Constraint".
+ * 
+ * @author Alexander Weitzmann
+ * @version 0.2
+ */
+public class ToggleDependenciesHandler extends ToggleDependenciesSuperHandler {
+
+	/**
+	 * Creates a new string-array, with same strings as given original, but the "transform-string". If
+	 * given transform-string is present, it will be removed, otherwise it will be added.<br>
+	 * Note that only the first found instance of given toggleString will be toggled, even if there
+	 * are more!
+	 * 
+	 * @param original
+	 *            The original array to be copied.
+	 * @param transformString
+	 *            String to add or remove.
+	 * @return Toggled string-array. That is: Same array, but with given toggle-string removed or
+	 *         added.
+	 */
+	@Override
+	String[] transformedCopy(String[] original, String transformString) {
+		boolean stringAbsent = true;
+
+		// Determine if string is present. If so, 'toggleIndex' will be corresponding index.
+		int toggleIndex = 0;
+		while (toggleIndex < original.length) {
+			if (original[toggleIndex].equals(transformString)) {
+				stringAbsent = false;
+				break;
+			}
+			++toggleIndex;
+		}
+
+		// Represents all dependencies after toggling;
+		String[] result;
+		if (stringAbsent) {
+			// append string
+			result = Arrays.copyOf(original, original.length + 1);
+			result[original.length] = transformString;
+		} else {
+			// remove string
+			result = new String[original.length - 1];
+			for (int i = 0; i < result.length; ++i) {
+				if (i >= toggleIndex) {
+					// skip string to be toggled
+					result[i] = original[i + 1];
+				} else {
+					result[i] = original[i];
+				}
+			}
+		}
+		return result;
+	}
+
+}
---
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/menuItems/SetDependencyEntries.java
@@ -0,0 +1,198 @@
+/**
+ *  License (BSD Style License):
+ *   Copyright (c) 2010
+ *   Author Patrick Jahnke
+ *   Software Engineering
+ *   Department of Computer Science
+ *   Technische Universitiät Darmstadt
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Engineering Group or Technische 
+ *     Universität Darmstadt nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this software without specific 
+ *     prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.tud.cs.st.vespucci.diagram.menuItems;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionEditPart;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.CompoundContributionItem;
+import org.eclipse.ui.menus.CommandContributionItem;
+import org.eclipse.ui.menus.CommandContributionItemParameter;
+
+import de.tud.cs.st.vespucci.io.ValidDependenciesReader;
+import de.tud.cs.st.vespucci.vespucci_model.Vespucci_modelPackage;
+
+/**
+ * This class provides the entries for the
+ * "Edit Constraint"/"Set Dependency"-menu. For each entry in the validDependencies-textfile
+ * ({@link de.tud.cs.st.vespucci/resources/validDependencies.txt}) one menu-entry will be generated.
+ * 
+ * @author Alexander Weitzmann
+ * @version 0.3
+ */
+public class SetDependencyEntries extends CompoundContributionItem {
+
+	/**
+	 * Valid names for dependencies read from the resource-file.
+	 */
+	private static final String[] dependencies = new ValidDependenciesReader().getKeywords();
+
+	/**
+	 * Descriptors for the check marks. There are two available check marks:
+	 * <UL>
+	 * <LI>Index 0: grey check mark, used to indicate a dependency, that is not set for all selected
+	 * constraints, but for at least one.
+	 * </UL>
+	 * <UL>
+	 * <LI>Index 1: black check mark, used to indicate a dependency, that is set for all selected
+	 * constraints.
+	 * </UL>
+	 * <UL>
+	 * <LI>Index 2: null, used to indicate a dependency, that is not set for all selected 
+	 * constraints.
+	 * </UL>
+	 */
+	private static final ImageDescriptor[] checkmark = new ImageDescriptor[3];
+
+	static {
+		checkmark[0] = new ImageDescriptor() {
+
+			@Override
+			public ImageData getImageData() {
+				final Image img = new Image(PlatformUI.getWorkbench().getDisplay(), this.getClass().getResourceAsStream(
+						"/resources/checkmark_grey.png"));
+				return img.getImageData();
+			}
+		};
+
+		checkmark[1] = new ImageDescriptor() {
+
+			@Override
+			public ImageData getImageData() {
+				final Image img = new Image(PlatformUI.getWorkbench().getDisplay(), this.getClass().getResourceAsStream(
+						"/resources/checkmark_black.png"));
+				return img.getImageData();
+			}
+		};
+		
+		checkmark[2] = null;
+	}
+
+	/**
+	 * This method traverses all selected constraints and returns, which check mark should be used,
+	 * 
+	 * @param dependency
+	 *            Dependency to be checked.
+	 * @return The index for the correct check mark in {@link #checkmark}.
+	 */
+	private static int getCheckMarkIndex(final String dependency) {
+		final IStructuredSelection selection = (IStructuredSelection) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getSelectionService().getSelection();
+
+		final Object[] selectionArr = selection.toArray();
+
+		// This array will contain the casted selection-objects.
+		final ConnectionEditPart[] selectedConnections = new ConnectionEditPart[selection.size()];
+
+		for (int i = 0; i < selection.size(); ++i) {
+			selectedConnections[i] = (ConnectionEditPart) selectionArr[i];
+		}
+
+		boolean selectionContainsDep = false;
+		boolean firstConstraint = true;
+		for (final ConnectionEditPart connection : selectedConnections) {
+			// Get dependencies of constraint a.k.a. name of connection
+			final EObject semanticConnection = connection.resolveSemanticElement();
+			final EStructuralFeature feature = semanticConnection.eClass().getEStructuralFeature(
+					Vespucci_modelPackage.CONNECTION__NAME);
+			final String conName = (String) semanticConnection.eGet(feature);
+
+			// check if given dependency is set
+			boolean conContainsDep = false;
+			for (final String str : conName.split(", ")) {
+				if (dependency.equals(str)) {
+					if (!firstConstraint && !selectionContainsDep) {
+						// dependency set, but there are constraints without this dependency
+						return 0;
+					}
+					selectionContainsDep = true;
+					conContainsDep = true;
+					break;
+				}
+			}
+			if (!firstConstraint && selectionContainsDep && !conContainsDep) {
+				// dependency not set, but there are constraints with this dependency
+				return 0;
+			}
+
+			firstConstraint = false;
+		}
+
+		if (selectionContainsDep) {
+			// all constraints have the given dependency set.
+			return 1;
+		}
+		// 2 indicates, that no icon will be used i.e. entry not checked.
+		return 2;
+	}
+
+	@Override
+	protected IContributionItem[] getContributionItems() {
+		// generate entries
+		final IContributionItem[] entries = new CommandContributionItem[dependencies.length];
+
+		for (int i = 0; i < dependencies.length; i++) {
+			final String dependency = dependencies[i];
+
+			final int checkMarkIndex = getCheckMarkIndex(dependency);
+			final String command;
+			if (checkMarkIndex == 0) {
+				// set command to set dependency for all constraints
+				command = "de.tud.cs.st.vespucci.diagram.setDependenciesCommand";
+			} else {
+				// set command to toggle dependency for all constraints
+				command = "de.tud.cs.st.vespucci.diagram.toggleDependenciesCommand";
+			}
+
+			final CommandContributionItemParameter contributionParameter = new CommandContributionItemParameter(PlatformUI
+					.getWorkbench().getActiveWorkbenchWindow(),
+					"de.tud.cs.st.vespucci.diagram.menuItems.SetDependencyContribution_" + dependency, command,
+					CommandContributionItem.STYLE_CHECK);
+			contributionParameter.label = dependency;
+			contributionParameter.icon = checkmark[checkMarkIndex];
+
+			entries[i] = new CommandContributionItem(contributionParameter);
+		}
+
+		return entries;
+	}
+}
