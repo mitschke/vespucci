e17bcd37281357e35184940ce2332b0630a50109
Dominic Scheurer <dom-scheurer@gmx.de>
2011-08-04 10:43:05 +0200

    Refactoring: Removed unneccessary helper methods.

--- de.tud.cs.st.vespucci/src/de/tud/cs/st/vespucci/transformation/TransformationHelperLibrary.java
+++ de.tud.cs.st.vespucci/src/de/tud/cs/st/vespucci/transformation/TransformationHelperLibrary.java
@@ -1,23 +1,17 @@
 package de.tud.cs.st.vespucci.transformation;
 
-import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
 
-import org.eclipse.gmf.runtime.notation.impl.ShapeImpl;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 
 import de.tud.cs.st.vespucci.vespucci_model.Connection;
 import de.tud.cs.st.vespucci.vespucci_model.Shape;
 import de.tud.cs.st.vespucci.vespucci_model.ShapesDiagram;
-import de.tud.cs.st.vespucci.vespucci_model.impl.EnsembleImpl;
 import de.tud.cs.st.vespucci.vespucci_model.impl.ShapesDiagramImpl;
 
 public class TransformationHelperLibrary {
-//	private static Deque<Shape> ensembles = new LinkedList<Shape>();
 	private static HashMap<Shape, Shape> modelShapes = new HashMap<Shape, Shape>();
 	private static HashMap<Connection, Connection> connections = new HashMap<Connection, Connection>();
-	private static HashMap<ShapeImpl, ShapeImpl> notationShapes = new HashMap<ShapeImpl, ShapeImpl>();
 	private static ShapesDiagram shapesDiagram;
 	
 	@Operation(contextual=true)
@@ -42,12 +36,6 @@ public class TransformationHelperLibrary {
 	}
 	
 	@Operation(contextual=true)
-	public static void rememberShape(Object result, Object self)
-	{
-		notationShapes.put((ShapeImpl)self, (ShapeImpl)result);
-	}
-	
-	@Operation(contextual=true)
 	public static ShapesDiagram getRememberedShapesDiagram(Object self) 
 	{
 		return shapesDiagram;
@@ -63,11 +51,5 @@ public class TransformationHelperLibrary {
 	public static Connection getRememberedConnection(Object self) 
 	{
 		return connections.get((Connection)self);
-	}
-	
-	@Operation(contextual=true)
-	public static Object getRememberedNotationShape(Object self) 
-	{
-		return notationShapes.get((ShapeImpl)self);
 	}
 }
