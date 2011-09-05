cceccfba0ed2464526fc53e6a943aaa8957ea79f
Thomas Schulz <ironman87@gmx.de>
2011-09-05 17:04:05 +0200

    CodeReview PrologFileCreator splitted (unfinished)

---
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/creator/EnsemblePrologFacts.java
@@ -0,0 +1,226 @@
+package de.tud.cs.st.vespucci.diagram.creator;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.emf.common.util.EList;
+
+import de.tud.cs.st.vespucci.vespucci_model.Dummy;
+import de.tud.cs.st.vespucci.vespucci_model.Ensemble;
+import de.tud.cs.st.vespucci.vespucci_model.Shape;
+
+public class EnsemblePrologFacts {
+
+	/**
+	 * Regular expression to split parameter names of ensembles.
+	 */
+	private static final Pattern PARAMETER_NAMES = Pattern.compile("(.*?)=(.*)");
+
+	
+	/**
+	 * Regular expression to match a name of an ensemble that contains parameter.
+	 */
+	private static final Pattern PARAMETER_LIST = Pattern.compile("^.+?" + // match the descriptor
+			"\\(" + // match the first bracket
+			"(.*)" + // match anything in between as group
+			"\\)$"); // match the last parenthesis by asserting the string ends here
+
+	
+	/**
+	 * Regular expression to check if argument starts with an upper case letter. E.g. a parameter
+	 * variable.
+	 */
+	private static final Pattern FIRST_LETTER_IS_UPPER_CASE = Pattern.compile("\\p{Upper}.*");
+	
+	/**
+	 * Name of the current diagram file.
+	 */
+	private String diagramFileName;
+	
+	/**
+	 * Search the diagram recursively and create all ensemble facts, except Dummy.
+	 * 
+	 * @param shapeList
+	 *            The list of shapes in the diagram.
+	 * @throws Exception
+	 * @return Returns the formatted ensemble facts.
+	 */
+	static StringBuilder createEnsembleFacts(final List<Shape> shapeList, String diagramFileName) throws Exception {
+		final StringBuilder ensembleFacts = new StringBuilder();
+		for (final Shape shape : shapeList) {
+			// create Ensemble facts:
+			if (shape instanceof Ensemble && shape != null) {
+				final Ensemble ensemble = (Ensemble) shape;
+				if (isAbstractEnsemble(ensemble)) {
+					ensembleFacts.append("abstract_ensemble");
+				} else {
+					ensembleFacts.append("ensemble");
+				}
+
+				//TODO: This is a workaround - invent a platform independent solution
+				final String query = ensemble.getQuery().replaceAll("\\p{Space}", " ");
+
+				ensembleFacts.append(String.format("('%s', %s, %s, (%s), [%s]).\n", diagramFileName,
+						createEnsembleDescriptor(ensemble), createEnsembleParameters(ensemble), query,
+						listSubEnsembles(ensemble.getShapes())));
+
+				// do children exist
+				if ((ensemble.getShapes() != null) && (ensemble.getShapes().size() > 0)) {
+					ensembleFacts.append(createEnsembleFacts(ensemble.getShapes(), diagramFileName));
+				}
+
+			}
+
+		}
+		return ensembleFacts;
+	}
+	
+	/**
+	 * @param ensemble
+	 * @return Return true, only if the ensemble is abstract, i.e. the ensemble contains at least
+	 *         one parameter
+	 *         variable.
+	 */
+	private static boolean isAbstractEnsemble(final Ensemble ensemble) {
+		final String[] parameters = splitEnsembleParameterList(ensemble);
+		for (final String parameter : parameters) {
+			if (FIRST_LETTER_IS_UPPER_CASE.matcher(parameter).matches()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param shape
+	 * @return The name of the ensemble in apostrophes. (Without parameters)
+	 */
+	static String createEnsembleDescriptor(final Shape shape) {
+		String name = "";
+		if (shape.getName() != null) {
+			name = shape.getName().length() == 0 ? "non-editpart" : shape.getName();
+		}
+		final StringBuilder s = new StringBuilder("'");
+		if (name.indexOf('(') > 0) {
+			s.append(name.subSequence(0, name.indexOf('(')));
+		} else {
+			s.append(name);
+		}
+		s.append("'");
+		return s.toString();
+	}
+	
+	/**
+	 * @param ensemble
+	 *            The ensemble whose parameters shall be extracted.
+	 * @return A prolog list of the form ['ParamName'=ParamName, ...]
+	 */
+	private static String createEnsembleParameters(final Ensemble ensemble) {
+		final String[] parameters = splitEnsembleParameterList(ensemble);
+		if (parameters.length == 0) {
+			return "[]";
+		}
+		final StringBuilder s = new StringBuilder("[");
+		s.append(createEncodedParameter(parameters[0]));
+		for (int i = 1; i < parameters.length; i++) {
+			s.append(", ");
+			s.append(createEncodedParameter(parameters[i]));
+		}
+		s.append("]");
+		return s.toString();
+	}
+	
+	/**
+	 * Create a formatted string with all given ensembles.
+	 * 
+	 * @param ensembles
+	 * @return Return formatted string listing the given ensembles.
+	 */
+	private static String listSubEnsembles(final EList<Shape> ensembles) {
+		final StringBuilder strBuilder = new StringBuilder();
+		if (ensembles == null) {
+			return strBuilder.toString();
+		}
+
+		String komma = "";
+		for (final Shape shape : ensembles) {
+			if (shape instanceof Dummy) {
+				strBuilder.append(komma + "'empty'");
+			} else if (shape instanceof Ensemble) {
+				strBuilder.append(komma + "'" + shape.getName() + "'");
+			}
+			komma = ", ";
+		}
+		return strBuilder.toString();
+	}
+	
+	/**
+	 * Returns the parameter definitions of an ensemble. I.e. the parameter list split at ", ".
+	 * 
+	 * @param ensemble
+	 * @return Return an array containing the parameters of given ensemble.
+	 */
+	private static String[] splitEnsembleParameterList(final Ensemble ensemble) {
+		if (ensemble.getName() == null) {
+			ensemble.setName("");
+		}
+		final String name = ensemble.getName().length() == 0 ? "non-editpart" : ensemble.getName();
+		final Matcher m = PARAMETER_LIST.matcher(name);
+		if (!m.matches()) {
+			return new String[0];
+		}
+		final List<String> parameterDefinitions = new LinkedList<String>();
+		final String parameters = m.group(1);
+		int start = 0;
+		int matchParenthesis = 0;
+		for (int i = 0; i < parameters.length(); i++) {
+			if (parameters.charAt(i) == '(') {
+				matchParenthesis++;
+			}
+			if (matchParenthesis > 0 && parameters.charAt(i) == ')') {
+				matchParenthesis--;
+			}
+			if (parameters.charAt(i) == ',' && matchParenthesis == 0) {
+				parameterDefinitions.add(parameters.substring(start, i).trim());
+				start = i + 1;
+			}
+		}
+		parameterDefinitions.add(parameters.substring(start, parameters.length()).trim());
+		final String[] result = new String[parameterDefinitions.size()];
+		return parameterDefinitions.toArray(result);
+	}
+	
+	/**
+	 * @param parameter
+	 * @return Return the encoded parameter.
+	 */
+	private static String createEncodedParameter(final String parameter) {
+		final StringBuilder s = new StringBuilder();
+		if (FIRST_LETTER_IS_UPPER_CASE.matcher(parameter).matches()) {
+			s.append("'");
+			s.append(parameter);
+			s.append("'");
+			s.append("=");
+			s.append(parameter);
+			return s.toString();
+		}
+		final Matcher m = PARAMETER_NAMES.matcher(parameter);
+		if (m.matches()) {
+			s.append("'");
+			s.append(m.group(1));
+			s.append("'");
+			s.append("=");
+			s.append(m.group(2));
+			return s.toString();
+		}
+
+		s.append("_");
+		s.append("=");
+		s.append(parameter);
+		return s.toString();
+	}
+
+}
--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/creator/PrologFileCreator.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/creator/PrologFileCreator.java
@@ -40,23 +40,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
-import de.tud.cs.st.vespucci.vespucci_model.Connection;
 import de.tud.cs.st.vespucci.vespucci_model.Dummy;
-import de.tud.cs.st.vespucci.vespucci_model.Ensemble;
-import de.tud.cs.st.vespucci.vespucci_model.Expected;
-import de.tud.cs.st.vespucci.vespucci_model.InAndOut;
-import de.tud.cs.st.vespucci.vespucci_model.Incoming;
-import de.tud.cs.st.vespucci.vespucci_model.NotAllowed;
-import de.tud.cs.st.vespucci.vespucci_model.Outgoing;
 import de.tud.cs.st.vespucci.vespucci_model.Shape;
 import de.tud.cs.st.vespucci.vespucci_model.ShapesDiagram;
 
@@ -68,25 +57,6 @@ import de.tud.cs.st.vespucci.vespucci_model.ShapesDiagram;
  * @author Thomas Schulz
  */
 public class PrologFileCreator {
-
-	/**
-	 * Regular expression to check if argument starts with an upper case letter. E.g. a parameter
-	 * variable.
-	 */
-	private static final Pattern FIRST_LETTER_IS_UPPER_CASE = Pattern.compile("\\p{Upper}.*");
-
-	/**
-	 * Regular expression to split parameter names of ensembles.
-	 */
-	private static final Pattern PARAMETER_NAMES = Pattern.compile("(.*?)=(.*)");
-
-	/**
-	 * Regular expression to match a name of an ensemble that contains parameter.
-	 */
-	private static final Pattern PARAMETER_LIST = Pattern.compile("^.+?" + // match the descriptor
-			"\\(" + // match the first bracket
-			"(.*)" + // match anything in between as group
-			"\\)$"); // match the last parenthesis by asserting the string ends here
 
 	/**
 	 * Read the given diagram and create a prolog file.
@@ -169,13 +139,10 @@ public class PrologFileCreator {
 		// insert ensemble Header
 		strBuilder.append(InvariantPrologFacts.createEnsembleHeader());
 
-		// reset transaction counter
-		dependencyCounter = 1;
+		// final StringBuilder ensembleFacts = createEnsembleFacts(diagram.getShapes());
+		final StringBuilder ensembleFacts = EnsemblePrologFacts.createEnsembleFacts(diagram.getShapes(), diagramFileName);
 
-		final boolean containsDummy = hasDummy(diagram.getShapes());
-		final StringBuilder ensembleFacts = createEnsembleFacts(diagram.getShapes());
-
-		if (containsDummy) {
+		if (hasDummy(diagram.getShapes())) {
 			ensembleFacts.append("ensemble('" + diagramFileName + "',(empty),empty,[]).\n");
 		}
 
@@ -186,7 +153,7 @@ public class PrologFileCreator {
 		strBuilder.append(InvariantPrologFacts.createDependencyHeader());
 
 		// insert dependencies
-		strBuilder.append(createDependencyFacts(diagram.getShapes()));
+		strBuilder.append(DependencyPrologFacts.createDependencyFacts(diagram.getShapes(), diagramFileName));
 
 		return strBuilder.toString().getBytes();
 	}
@@ -206,203 +173,6 @@ public class PrologFileCreator {
 	}
 
 	/**
-	 * Search the diagram recursively and create all ensemble facts, except Dummy.
-	 * 
-	 * @param shapeList
-	 *            The list of shapes in the diagram.
-	 * @throws Exception
-	 * @return Returns the formatted ensemble facts.
-	 */
-	private StringBuilder createEnsembleFacts(final List<Shape> shapeList) throws Exception {
-		final StringBuilder ensembleFacts = new StringBuilder();
-		for (final Shape shape : shapeList) {
-			// create Ensemble facts:
-			if (shape instanceof Ensemble && shape != null) {
-				final Ensemble ensemble = (Ensemble) shape;
-				if (isAbstractEnsemble(ensemble)) {
-					ensembleFacts.append("abstract_ensemble");
-				} else {
-					ensembleFacts.append("ensemble");
-				}
-
-				//TODO: This is a workaround - invent a platform independent solution
-				final String query = ensemble.getQuery().replaceAll("\\p{Space}", " ");
-
-				ensembleFacts.append(String.format("('%s', %s, %s, (%s), [%s]).\n", diagramFileName,
-						createEnsembleDescriptor(ensemble), createEnsembleParameters(ensemble), query,
-						listSubEnsembles(ensemble.getShapes())));
-
-				// do children exist
-				if ((ensemble.getShapes() != null) && (ensemble.getShapes().size() > 0)) {
-					ensembleFacts.append(createEnsembleFacts(ensemble.getShapes()));
-				}
-
-			}
-
-		}
-		return ensembleFacts;
-	}
-
-	/**
-	 * Search the diagram recursive and create all facts.
-	 * 
-	 * @param shapeList
-	 * @return Returns the formatted dependency facts.
-	 * @author Patrick Jahnke
-	 */
-	private StringBuilder createDependencyFacts(final List<Shape> shapeList) {
-		final StringBuilder dependencyFacts = new StringBuilder();
-		for (final Shape shape : shapeList) {
-			if (shape instanceof Ensemble) {
-				final Ensemble ensemble = (Ensemble) shape;
-				if (shape != null) {
-					for (final Connection connection : ensemble.getTargetConnections()) {
-						dependencyFacts.append(createSingleDependencyFact(connection));
-					}
-				}
-				if (ensemble.getShapes() != null) {
-					dependencyFacts.append(createDependencyFacts(ensemble.getShapes()));
-				}
-
-			}
-		}
-
-		return dependencyFacts;
-	}
-
-	/**
-	 * @param parameter
-	 * @return Return the encoded parameter.
-	 */
-	private static String createEncodedParameter(final String parameter) {
-		final StringBuilder s = new StringBuilder();
-		if (FIRST_LETTER_IS_UPPER_CASE.matcher(parameter).matches()) {
-			s.append("'");
-			s.append(parameter);
-			s.append("'");
-			s.append("=");
-			s.append(parameter);
-			return s.toString();
-		}
-		final Matcher m = PARAMETER_NAMES.matcher(parameter);
-		if (m.matches()) {
-			s.append("'");
-			s.append(m.group(1));
-			s.append("'");
-			s.append("=");
-			s.append(m.group(2));
-			return s.toString();
-		}
-
-		s.append("_");
-		s.append("=");
-		s.append(parameter);
-		return s.toString();
-	}
-
-	/**
-	 * 
-	 * @param shape
-	 * @return The name of the ensemble in apostrophes. (Without parameters)
-	 */
-	private static String createEnsembleDescriptor(final Shape shape) {
-		String name = "";
-		if (shape.getName() != null) {
-			name = shape.getName().length() == 0 ? "non-editpart" : shape.getName();
-		}
-		final StringBuilder s = new StringBuilder("'");
-		if (name.indexOf('(') > 0) {
-			s.append(name.subSequence(0, name.indexOf('(')));
-		} else {
-			s.append(name);
-		}
-		s.append("'");
-		return s.toString();
-	}
-
-	/**
-	 * @param ensemble
-	 *            The ensemble whose parameters shall be extracted.
-	 * @return A prolog list of the form ['ParamName'=ParamName, ...]
-	 */
-	private static String createEnsembleParameters(final Ensemble ensemble) {
-		final String[] parameters = splitEnsembleParameterList(ensemble);
-		if (parameters.length == 0) {
-			return "[]";
-		}
-		final StringBuilder s = new StringBuilder("[");
-		s.append(createEncodedParameter(parameters[0]));
-		for (int i = 1; i < parameters.length; i++) {
-			s.append(", ");
-			s.append(createEncodedParameter(parameters[i]));
-		}
-		s.append("]");
-		return s.toString();
-	}
-
-	/**
-	 * @param shape
-	 * @return Returns the name of an ensemble (without the parameter).
-	 */
-	private static String getEnsembleName(final Shape shape) {
-		if (shape instanceof Ensemble) {
-			return PrologFileCreator.createEnsembleDescriptor(shape);
-		} else if (shape instanceof Dummy) {
-			return "empty";
-		}
-		return "not_defined";
-	}
-
-	/**
-	 * Note, that {@link Connection#getSource()} does not return the same. That's because the source
-	 * of the connection
-	 * will be set to the parent of the semantic source-ensemble, if a red line is used.
-	 * 
-	 * @param connection
-	 * @return Returns the source of given connection.
-	 */
-	private static Shape getSource(final Connection connection) {
-		if ((connection.getOriginalSource() == null) || (connection.getOriginalSource().size() == 0)) {
-			return connection.getSource();
-		} else {
-			return connection.getOriginalSource().get(0);
-		}
-	}
-
-	/**
-	 * Note, that {@link Connection#getTarget()} does not return the same. That's because the target
-	 * of the connection
-	 * will be set to the parent of the semantic target-ensemble, if a red line is used.
-	 * 
-	 * @param connection
-	 * @return Returns the source of given connection.
-	 */
-	private static Shape getTarget(final Connection connection) {
-		// Get the original target (and not the red line target)
-		if ((connection.getOriginalTarget() == null) || (connection.getOriginalTarget().size() == 0)) {
-			return connection.getTarget();
-		} else {
-			return connection.getOriginalTarget().get(0);
-		}
-	}
-
-	/**
-	 * @param ensemble
-	 * @return Return true, only if the ensemble is abstract, i.e. the ensemble contains at least
-	 *         one parameter
-	 *         variable.
-	 */
-	private static boolean isAbstractEnsemble(final Ensemble ensemble) {
-		final String[] parameters = splitEnsembleParameterList(ensemble);
-		for (final String parameter : parameters) {
-			if (FIRST_LETTER_IS_UPPER_CASE.matcher(parameter).matches()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
 	 * @param file
 	 * @return Return true only if the given file object is a diagram file.
 	 */
@@ -412,111 +182,8 @@ public class PrologFileCreator {
 	}
 
 	/**
-	 * Create a formatted string with all given ensembles.
-	 * 
-	 * @param ensembles
-	 * @return Return formatted string listing the given ensembles.
-	 */
-	private static String listSubEnsembles(final EList<Shape> ensembles) {
-		final StringBuilder strBuilder = new StringBuilder();
-		if (ensembles == null) {
-			return strBuilder.toString();
-		}
-
-		String komma = "";
-		for (final Shape shape : ensembles) {
-			if (shape instanceof Dummy) {
-				strBuilder.append(komma + "'empty'");
-			} else if (shape instanceof Ensemble) {
-				strBuilder.append(komma + "'" + shape.getName() + "'");
-			}
-			komma = ", ";
-		}
-		return strBuilder.toString();
-	}
-
-	/**
-	 * Returns the parameter definitions of an ensemble. I.e. the parameter list split at ", ".
-	 * 
-	 * @param ensemble
-	 * @return Return an array containing the parameters of given ensemble.
-	 */
-	private static String[] splitEnsembleParameterList(final Ensemble ensemble) {
-		if (ensemble.getName() == null) {
-			ensemble.setName("");
-		}
-		final String name = ensemble.getName().length() == 0 ? "non-editpart" : ensemble.getName();
-		final Matcher m = PARAMETER_LIST.matcher(name);
-		if (!m.matches()) {
-			return new String[0];
-		}
-		final List<String> parameterDefinitions = new LinkedList<String>();
-		final String parameters = m.group(1);
-		int start = 0;
-		int matchParenthesis = 0;
-		for (int i = 0; i < parameters.length(); i++) {
-			if (parameters.charAt(i) == '(') {
-				matchParenthesis++;
-			}
-			if (matchParenthesis > 0 && parameters.charAt(i) == ')') {
-				matchParenthesis--;
-			}
-			if (parameters.charAt(i) == ',' && matchParenthesis == 0) {
-				parameterDefinitions.add(parameters.substring(start, i).trim());
-				start = i + 1;
-			}
-		}
-		parameterDefinitions.add(parameters.substring(start, parameters.length()).trim());
-		final String[] result = new String[parameterDefinitions.size()];
-		return parameterDefinitions.toArray(result);
-	}
-
-	/**
-	 * Counter for dependencies in given diagram.
-	 */
-	private int dependencyCounter;
-
-	/**
 	 * Name of the current diagram file.
 	 */
 	private String diagramFileName;
-
-	/**
-	 * @param connection
-	 * @return Return a fact for a single dependency.
-	 */
-	private String createSingleDependencyFact(final Connection connection) {
-		final Shape source = getSource(connection);
-		final Shape target = getTarget(connection);
-
-		final StringBuilder transactionSB = new StringBuilder();
-		// TODO: delete next 2 lines if saved sad file doesn't
-		// contains copy/paste artifact references. Problem: if one copy from one sad file
-		// an Ensemble with dependency in another sad file than the copy of
-		// the Ensemble will contains a reference to original Ensemble in first sad file.
-		// This reference has no influence on working process but it has problem here,
-		// by converting to prolog facts
-		if (connection.getSource().eIsProxy() || connection.getTarget().eIsProxy()) {
-			return "";
-		}
-
-		final String dependencySuffix = String.format("('%s', %s, %s, [], %s, [], [%s]).\n", diagramFileName, dependencyCounter,
-				getEnsembleName(source), getEnsembleName(target), connection.getName());
-		if (connection instanceof Outgoing) {
-			transactionSB.append("outgoing").append(dependencySuffix);
-		} else if (connection instanceof Incoming) {
-			transactionSB.append("incoming").append(dependencySuffix);
-		} else if (connection instanceof Expected) {
-			transactionSB.append("expected").append(dependencySuffix);
-		} else if (connection instanceof NotAllowed) {
-			transactionSB.append("not_allowed").append(dependencySuffix);
-		} else if (connection instanceof InAndOut) {
-			transactionSB.append("incoming").append(dependencySuffix);
-			transactionSB.append("outgoing").append(dependencySuffix);
-		}
-
-		dependencyCounter++;
-		return transactionSB.toString();
-	}
 
 }
