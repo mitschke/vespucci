78d19742c7cc855284eecd7475aa725c7ea48072
Theo <theokischka@gmx.net>
2011-09-27 17:44:32 +0200

    US ProblemsView added

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/vespucci_model/diagram/providers/VespucciValidationProvider.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/vespucci_model/diagram/providers/VespucciValidationProvider.java
@@ -34,6 +34,7 @@
 package de.tud.cs.st.vespucci.vespucci_model.diagram.providers;
 
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -193,6 +194,139 @@ public class VespucciValidationProvider {
 				if (!valid) {
 					return ctx.createFailureStatus(String.format("Depdendency kind %s is invalid", context));
 				}
+			}
+			return ctx.createSuccessStatus();
+		}
+	}
+	
+	/**
+	 * @generated
+	 */
+	public static class Adapter6 extends AbstractModelConstraint {
+
+		/**
+		 * Checks if the brackets in an Ensemble Query are complete.
+		 * 
+		 * @author Theo Kischka
+		 * @generated NOT
+		 * @return Success-Status, if validation successful; Failure otherwise.
+		 */
+		public IStatus validate(IValidationContext ctx) {
+			de.tud.cs.st.vespucci.vespucci_model.Ensemble context = (de.tud.cs.st.vespucci.vespucci_model.Ensemble) ctx
+					.getTarget();
+			String query = context.getQuery();
+
+			// alle Klammern innerhalb Prolog-Strings rausfiltern (z.B. im Ensemblenamen)
+			query = filterPrologStringsRek(query);
+
+			if (!checkBrackets(query, '(', ')')) {
+				return ctx.createFailureStatus("Invalid parenthesis count in Ensemble query.");
+			}
+			return ctx.createSuccessStatus();
+		}
+
+		/**
+		 * Returns true, if brackets are typed properly.
+		 * 
+		 * @generated NOT
+		 */
+		protected static boolean checkBrackets(String query, char openingBracket, char closingBracket) {
+			Stack<Character> openBrackets = new Stack<Character>();
+
+			for (int i = 0; i < query.length(); i++) {
+				char c = query.charAt(i);
+
+				if (c == openingBracket) {
+					openBrackets.push(c);
+				}
+				if (c == closingBracket) {
+					if (openBrackets.isEmpty()) {
+						return false;
+					} else {
+						openBrackets.pop();
+					}
+				}
+			}
+
+			if (!openBrackets.isEmpty()) {
+				return false;
+			}
+			return true;
+		}
+
+		/**
+		 * Filters everything between two 's within a string out.
+		 * 
+		 * @generated NOT
+		 */
+		protected static String filterPrologStringsRek(String s) {
+			int beginIndex = s.indexOf('\'');
+			int endIndex = s.length() - 1;
+			if (beginIndex == -1) {
+				return s;
+			} else {
+				endIndex = s.replaceFirst("'", " ").indexOf('\'');
+				if (endIndex == -1) {
+					return s;
+				} else {
+					return s.substring(0, beginIndex) + filterPrologStringsRek(s.substring(endIndex, s.length()));
+				}
+			}
+		}
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Adapter7 extends AbstractModelConstraint {
+
+		/**
+		 * Checks if Ensemble query prolog strings are complete.
+		 * E.g.: package('de.tud.cs.se.flashcards.ui') is correct while
+		 * package(de.tud.cs.se.flashcards.ui') is not.
+		 * 
+		 * @author Theo Kischka
+		 * @generated NOT
+		 * @return Success-Status, if validation successful; Failure otherwise.
+		 */
+		public IStatus validate(IValidationContext ctx) {
+			de.tud.cs.st.vespucci.vespucci_model.Ensemble context = (de.tud.cs.st.vespucci.vespucci_model.Ensemble) ctx
+					.getTarget();
+			String query = context.getQuery();
+			boolean stringOpen = false;
+
+			for (int i = 0; i < query.length(); i++) {
+				char c = query.charAt(i);
+				if (c == '\'') {
+					stringOpen = !stringOpen;
+				}
+			}
+			if (stringOpen) {
+				return ctx.createFailureStatus("Missing ' in Ensemble query.");
+			}
+			return ctx.createSuccessStatus();
+		}
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Adapter8 extends AbstractModelConstraint {
+
+		/**
+		 * @author Theo Kischka, Thomas Schulz
+		 * @generated NOT
+		 */
+		public IStatus validate(IValidationContext ctx) {
+			de.tud.cs.st.vespucci.vespucci_model.Ensemble context = (de.tud.cs.st.vespucci.vespucci_model.Ensemble) ctx
+					.getTarget();
+			String query = context.getQuery();
+
+			// alle Klammern innerhalb Prolog-Strings rausfiltern (z.B. im Ensemblenamen)
+			query = Adapter6.filterPrologStringsRek(query);
+
+			if (!Adapter6.checkBrackets(query, '[', ']')) {
+				return ctx.createFailureStatus("Invalid brackets count in Ensemble query.");
 			}
 			return ctx.createSuccessStatus();
 		}
