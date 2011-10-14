13f0c3a94ae17e40ab08ffbe77aebf3d39b28e77
Olav Lenz <olav.lenz@gmail.com>
2011-10-14 15:05:26 +0200

    Add comments

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/processing/Util.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/processing/Util.java
@@ -1,3 +1,36 @@
+/*
+ *  License (BSD Style License):
+ *   Copyright (c) 2011
+ *   Software Engineering
+ *   Department of Computer Science
+ *   Technische Universitiät Darmstadt
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Engineering Group or Technische
+ *     Universität Darmstadt nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific
+ *     prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st.vespucci.diagram.processing;
 
 import java.io.File;
@@ -19,6 +52,12 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.ui.statushandlers.StatusManager;
 
+/**
+ * Some static helpful methods
+ * 
+ * @author Patrick Gottschämmer
+ * @author Olav Lenz
+ */
 public class Util {
 
 	private static final String PLUGIN_ID = "de.tud.cs.st.vespucci.diagram";
@@ -62,6 +101,8 @@ public class Util {
 	}
 	
 	/**
+	 * Checks if File has a passed extension
+	 * 
 	 * @param file File to check
 	 * @param extension Extension of the file type to check
 	 * @return Return true only if the given file extension is equals to the parameter extension
@@ -73,6 +114,14 @@ public class Util {
 		return (ext.equals(extension));
 	}
 	
+	/**
+	 * Returns a list of IFiles of a passed IProject, filtered by file type with enum Selection 
+	 * 
+	 * @param project The IProject to search for IFiles
+	 * @param selection Filtered file type by enum Selection (e.g. Selection.CLASS for only *.class files)
+	 * @return List of IFiles
+	 * @See {@link Selection} 
+	 */
 	public static LinkedList<IFile> getFilesOfProject(IProject project, Selection selection){
 		LinkedList<IFile> ifiles = new LinkedList<IFile>();
 		switch(selection){
@@ -83,6 +132,12 @@ public class Util {
 		return ifiles;
 	}
 	
+	/**
+	 * Returns a list of Files with .jar libraries of a passed IProject.
+	 * 
+	 * @param project The IProject to search for Files
+	 * @return List of Files
+	 */
 	public static LinkedList<File> getLinkedLiberiesFiles(IProject project) {
 		LinkedList<File> elements = new LinkedList<File>();
 
