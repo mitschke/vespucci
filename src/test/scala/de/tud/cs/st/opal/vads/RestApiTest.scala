593aa41bc065b92a5fad0c2ff6c47ef6aa4ea2c6
Mateusz Parzonka <parzonka@gmail.com>
2011-11-22 22:38:17 +0100

    Cleanup. Tests run with JUnit-Runner in Eclipse.

--- src/test/scala/de/tud/cs/st/opal/vads/RestApiTest.scala
+++ src/test/scala/de/tud/cs/st/opal/vads/RestApiTest.scala
@@ -3,9 +3,9 @@ package de.tud.cs.st.opal.vads
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.apache.http.client.methods.HttpUriRequest
-import org.apache.http.client.{ResponseHandler, HttpClient}
+import org.apache.http.client.{ ResponseHandler, HttpClient }
 import org.apache.http.protocol.HttpContext
-import org.apache.http.{HttpRequest, HttpHost}
+import org.apache.http.{ HttpRequest, HttpHost }
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
@@ -14,9 +14,9 @@ class RestApiTest extends FlatSpec with ShouldMatchers {
 
   import dispatch._
   import org.apache.http.protocol.HTTP.CONTENT_ENCODING
-  
-  "1 + 1" should "equal 2" in {
-    1 + 1  should equal { 2 }
+
+   "The Server" should "display a plain text message on GET" in {
+    
   }
   
 }
