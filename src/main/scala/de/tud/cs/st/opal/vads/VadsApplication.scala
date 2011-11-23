7112411df12a639d4bdd9ef785a0587d4b54001d
Mateusz Parzonka <parzonka@gmail.com>
2011-11-23 01:21:14 +0100

    Server is now started inside the test suite.

--- src/main/scala/de/tud/cs/st/opal/vads/VadsApplication.scala
+++ src/main/scala/de/tud/cs/st/opal/vads/VadsApplication.scala
@@ -8,7 +8,7 @@ import org.dorest.server.log.ConsoleLogging
  */
 object VespucciArchitectureDescriptionServer
   extends Server(9000)
-  with scala.App
+//  with scala.App
   with ConsoleLogging // TODO needs to exchanged
   {
 
--- src/test/scala/de/tud/cs/st/opal/vads/RestApiTest.scala
+++ src/test/scala/de/tud/cs/st/opal/vads/RestApiTest.scala
@@ -3,6 +3,7 @@ package de.tud.cs.st.opal.vads
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.ShouldMatchers
 
 import org.apache.http.client.methods.HttpUriRequest
@@ -11,7 +12,12 @@ import org.apache.http.protocol.HttpContext
 import org.apache.http.{ HttpRequest, HttpHost }
 
 @RunWith(classOf[JUnitRunner])
-class RestApiTest extends FlatSpec with ShouldMatchers {
+class RestApiTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
+
+  override def beforeAll(configMap: Map[String, Any]) {
+    import de.tud.cs.st.opal.vads.VespucciArchitectureDescriptionServer
+    VespucciArchitectureDescriptionServer
+  }
 
   import dispatch._
   import Http._
@@ -19,9 +25,9 @@ class RestApiTest extends FlatSpec with ShouldMatchers {
   "The root resource" should "return plain text on GET" in {
     Http(url("http://localhost:9000") <:< Map("Accept" -> "text/plain") as_str) should equal { "Hello!" }
   }
-
-  it should "return HTML on GET"
-
-  it should "return JSON on GET"
-
+  
+//  it should "return HTML on GET"
+//
+//  it should "return JSON on GET"
+  
 }
