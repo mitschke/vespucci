4e9ab482755fd8d651722c0b6dd634bc03c06481
Mateusz Parzonka <parzonka@gmail.com>
2011-11-24 13:17:24 +0100

    Renamed test and implemented POST operation with test.

--- src/main/scala/de/tud/cs/st/opal/vads/VADServer.scala
+++ src/main/scala/de/tud/cs/st/opal/vads/VADServer.scala
@@ -10,21 +10,46 @@ class VADServer
   extends Server(9000)
   with ConsoleLogging // TODO needs to exchanged
   {
+  
+  def root = "/"
 
   this register new HandlerFactory[Root] {
-    path {"/"}
+    path {root}
     def create = new Root
+  }
+  
+   this register new HandlerFactory[Descriptions] {
+    path {root :: "descriptions"}
+    def create = new Descriptions
+  }
+   
+    this register new HandlerFactory[Description] {
+    path {root :: "descriptions" :: LongValue((desc, longid) => desc.id = longid)}
+    def create = new Description
   }
   
   start()
 }
 
-import org.dorest.server.rest.{ RESTInterface, TEXTSupport }
+import org.dorest.server.rest._
 
 class Root extends RESTInterface with TEXTSupport {
 
   get returns TEXT { "Hello!" }
 
+}
+
+class Descriptions extends RESTInterface with TEXTSupport with XMLSupport{
+  
+    post of XML returns XML { <success><id>42</id></success> }
+
+}
+
+class Description extends RESTInterface with TEXTSupport with XMLSupport {
+  
+  var id: Long = _
+  
+  
 }
 
 object VADServerApp extends VADServer with scala.App
--- src/test/scala/de/tud/cs/st/opal/vads/VADServerTest.scala
+++ src/test/scala/de/tud/cs/st/opal/vads/VADServerTest.scala
@@ -15,7 +15,7 @@ import dispatch.{ Http => DispatchHttp, _ }
 import DispatchHttp._
 
 @RunWith(classOf[JUnitRunner])
-class RestApiTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
+class VADServerTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
 
   override def beforeAll(configMap: Map[String, Any]) {
 
@@ -23,20 +23,20 @@ class RestApiTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
     new VADServer
   }
 
-  "The root resource" should "return plain text on GET" in {
+  "The root resource" should "return a welcome in plain text on GET" in {
     Http(url("http://localhost:9000") <:< Map("Accept" -> "text/plain") as_str) should equal { "Hello!" }
   }
 
-  //  it should "return HTML on GET"
-  //
-  //  it should "return JSON on GET"
+  "The descriptions resource" should "create a new description on POST via XML" in {
+    Http(url("http://localhost:9000/descriptions") <:< Map("Accept" -> "application/xml") << "<xml/>" </> { nodes => (nodes \\ "id").text}) should equal { "42" }
+  }
 
 }
 
 /**
  * Enables control over logging.
  */
- object Http extends DispatchHttp {
+object Http extends DispatchHttp {
 
   override def make_logger = new Logger {
     def info(msg: String, items: Any*) {
