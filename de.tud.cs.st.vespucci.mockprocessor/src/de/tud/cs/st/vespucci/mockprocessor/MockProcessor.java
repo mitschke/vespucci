d3dddd53b9363d5376f49ea67a103c97aa9e5ff0
Olav Lenz <olav.lenz@gmail.com>
2011-12-11 17:55:09 +0100

    add license text first stable version of de.tud.cs.st.vespucci.marker

--- de.tud.cs.st.vespucci.mockprocessor/src/de/tud/cs/st/vespucci/mockprocessor/MockProcessor.java
+++ de.tud.cs.st.vespucci.mockprocessor/src/de/tud/cs/st/vespucci/mockprocessor/MockProcessor.java
@@ -1,6 +1,38 @@
+/*
+ *  License (BSD Style License):
+ *   Copyright (c) 2011
+ *   Software Technology Group
+ *   Department of Computer Science
+ *   Technische Universitiät Darmstadt
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Technology Group Group or Technische
+ *     Universität Darmstadt nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific
+ *     prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st.vespucci.mockprocessor;
 
-import java.util.LinkedList;
 import java.util.Set;
 
 import de.tud.cs.st.vespucci.diagram.processing.IModelProcessor;
@@ -59,11 +91,11 @@ public class MockProcessor implements IModelProcessor {
 		
 		ISourceClass mainView = new SourceClass("view", "MainView");
 		
-		LinkedList<String> paramTypes = new LinkedList<String>();
-		IMethodElement dataModel_createView = new MethodElement("model", "DataModel", "createView", "void", paramTypes);
+		String[] paramTypes = new String[0];
+		IMethodElement dataModel_createView = new MethodElement("model", "DataModel", "createView", "V", paramTypes);
 		
-		paramTypes = new LinkedList<String>();
-		IMethodElement mainController_doSome = new MethodElement("controller", "MainController", "doSome", "void", paramTypes);
+		paramTypes = new String[0];
+		IMethodElement mainController_doSome = new MethodElement("controller.test", "MainController", "doSome", "V", paramTypes);
 
 		IViolation firstViolation = new Violation(
 				"Not allowed call from Model to Controller",
