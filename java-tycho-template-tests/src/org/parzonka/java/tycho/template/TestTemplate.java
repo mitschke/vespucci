055c9e5095ff179ae0cb9fa5c63ced0b890b1ae5
Mateusz Parzonka <parzonka@gmail.com>
2012-01-14 02:43:15 +0100

    Added eclipse-test-plugin.

---
+++ java-tycho-template-tests/src/org/parzonka/java/tycho/template/TestTemplate.java
@@ -0,0 +1,15 @@
+package org.parzonka.java.tycho.template;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestTemplate {
+
+    @Test
+    public void someTest() {
+    	
+    	assertTrue(true);
+    	
+    }
+}
