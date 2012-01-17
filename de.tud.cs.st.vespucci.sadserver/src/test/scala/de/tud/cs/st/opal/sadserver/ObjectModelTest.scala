265c1576706caf8fbbc6d3fc8b6591ff4d34fa1b
Mateusz Parzonka <parzonka@gmail.com>
2012-01-17 02:36:35 +0100

    Fixed test.

--- de.tud.cs.st.vespucci.sadserver/src/test/scala/de/tud/cs/st/opal/sadserver/ObjectModelTest.scala
+++ de.tud.cs.st.vespucci.sadserver/src/test/scala/de/tud/cs/st/opal/sadserver/ObjectModelTest.scala
@@ -8,18 +8,20 @@ import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
 class ObjectModelTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
-  
+
+  GlobalProperties.loadPropertiesFile("src/test/resources/sadserver-test.properties")
+
   override def beforeAll(configMap: Map[String, Any]) {
     println("Starting ObjectModelTests")
   }
-  
-   "Description" should "be constructed correctly from XML" in {
-   val desc = Description(scala.xml.XML.loadFile("src/test/resources/existingDescription.xml"))
-   desc.id should equal { "f81d4fae-7dec-11d0-a765-00a0c91e6bf6" }
-   desc.name should equal { "myName" }
-   desc.`type` should equal { "myType" }
-   println(desc.toXML)
-   
+
+  "Description" should "be constructed correctly from XML" in {
+    val desc = Description(scala.xml.XML.loadFile("src/test/resources/existingDescription.xml"))
+    desc.id should equal { "f81d4fae-7dec-11d0-a765-00a0c91e6bf6" }
+    desc.name should equal { "myName" }
+    desc.`type` should equal { "myType" }
+    println(desc.toXML)
+
   }
-  
+
 }
