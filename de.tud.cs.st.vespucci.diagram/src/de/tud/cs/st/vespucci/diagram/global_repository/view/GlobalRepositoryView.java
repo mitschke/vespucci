5edceebaa1289e09d07d0e27cab838385ffc407e
Tabea Born <tabeaborn@googlemail.com>
2012-02-08 15:20:09 +0100

    einige Korrekturen

--- de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/global_repository/view/GlobalRepositoryView.java
+++ de.tud.cs.st.vespucci.diagram/src/de/tud/cs/st/vespucci/diagram/global_repository/view/GlobalRepositoryView.java
@@ -1,43 +1,62 @@
+/**
+ *  License (BSD Style License):
+ *   Copyright (c) 2012
+ *   Software Engineering
+ *   Department of Computer Science
+ *   Technische Universität Darmstadt
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the Software Engineering Group or Technische 
+ *     Universität Darmstadt nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this software without specific 
+ *     prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package de.tud.cs.st.vespucci.diagram.global_repository.view;
 
-
-
 import org.eclipse.jface.action.Action;
-
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
-
 import org.eclipse.jface.resource.ImageDescriptor;
-
-import org.eclipse.jface.viewers.ListViewer;
-
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.*;
-
-import org.eclipse.ui.PlatformUI;
-
-import org.eclipse.ui.menus.IMenuService;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
-
 
 import de.tud.cs.st.vespucci.vespucci_model.diagram.part.VespucciDiagramEditorPlugin;
 
-
-
-
-
 /**
- * @author Tanya Harizanova
+ * @author Tanya Harizanova, Tabea Born, Christian Knapp
  * View class makes the Global Repository View
  */
 
 public class GlobalRepositoryView  extends ViewPart{
 	
+	//TODO: EditImage
 	private static final String ADD_IMAGE = "icons/global_repository/AddEnsemble.gif";
 	private static final String DELETE_IMAGE="icons/global_repository/DeleteEnsemble.gif";
-	private static final String SELECT_IMAGE="icons/global_repository/SelectAllEnsemble.gif";
+	private static final String EDIT_IMAGE="icons/global_repository/SelectAllEnsemble.gif";
 	private Label label;
-	private Action addItemAction, deleteItemAction, selectAllAction;
+	private Action addItemAction, deleteItemAction, editAction;
 	
 	@Override
 	public void createPartControl(Composite parent) {
@@ -73,13 +92,13 @@ public class GlobalRepositoryView  extends ViewPart{
            //set image icon for this action
            deleteItemAction.setImageDescriptor(ImageDescriptor.createFromImage(getImage(DELETE_IMAGE)));
 
-           selectAllAction = new Action("Select All") {
+           editAction = new Action("Edit") {
                    public void run() {
-                           //selectAll();
+                           //edit();
                    }
            };
            //set image icon for this action
-           selectAllAction.setImageDescriptor(ImageDescriptor.createFromImage(getImage(SELECT_IMAGE)));
+           editAction.setImageDescriptor(ImageDescriptor.createFromImage(getImage(EDIT_IMAGE)));
     }
 
 
@@ -90,7 +109,7 @@ public class GlobalRepositoryView  extends ViewPart{
             IToolBarManager mgr = getViewSite().getActionBars().getToolBarManager();
             mgr.add(addItemAction);
             mgr.add(deleteItemAction);
-            mgr.add(selectAllAction);
+            mgr.add(editAction);
     }
 
     /**
